2e05d77470dc888936ef8d5df68a98c82423ec97
HamzaKAVAS <hmzkavas@gmail.com>
2025-05-02 17:49:59 +0300

    020525

--- src/test/java/manage/gulay/AddCities.java
+++ src/test/java/manage/gulay/AddCities.java
@@ -4,4 +4,13 @@ public class AddCities {
 
     private String citiesInsert= "insert into cities(id,name,state_id,status,created_at) values(1, 'Abadan', 1715, 1, now());";
     private String getAfterCitiesInsert ="select id, name, state_id, status, created_at from cities where id = 1;";
+
+    // getter
+    public String citiesInsert() {
+        return citiesInsert;
+    }
+
+    public String getAfterCitiesInsert() {
+        return getAfterCitiesInsert;
+    }
 }
--- src/test/java/stepdefinitions/DB/Add_Cities_Step.java
+++ src/test/java/stepdefinitions/DB/Add_Cities_Step.java
@@ -1,5 +1,7 @@
 package stepdefinitions.DB;
 
+import manage.gulay.AddCities;
+
 public class Add_Cities_Step {
 
     AddCities add_cities = new AddCities();
